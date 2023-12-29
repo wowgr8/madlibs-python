3c1582d25ef800739f0dc5694a1e79412c62c2cd
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-28 20:29:36 -0800

    add char target variables, add loop detecting brackets in story

--- madlibs.py
+++ madlibs.py
@@ -1,4 +1,26 @@
 with open("story.txt", "r") as f: 
   story = f.read()
 
-print(story)
+words = []
+start_of_word = -1
+
+target_start = "<"
+target_end = ">"
+
+# Loop through characters in the 'story' string and enumerate provides both position (i) and character (char) in the position
+for i, char in enumerate(story):
+    # Check if the character is the start of the target word
+    if char == target_start:
+        start_of_word = i
+
+    # Check if the character is the end of the target word and there's a valid start position
+    if char == target_end and start_of_word != -1:
+        # Extract the word from start position to the current position (inclusive)
+        word = story[start_of_word: i + 1]
+        # Append the extracted word to the 'words' list
+        words.append(word)
+        # Reset the start position for the next word
+        start_of_word = -1
+
+
+print(words)
